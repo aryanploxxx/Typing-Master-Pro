f8fe67a25967bbcf0abe49eebb84742a98ea2831
aryanploxxx <aryangupta8291@gmail.com>
2024-06-04 00:21:00 +0530

    UI Improvement

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-let promptt = "tailwind css is the best";
+let promptt = "this is a random prompt this is a random prompt this is a random prompt this is a random prompt this is a random prompt this is a random prompt";
 
 window.addEventListener('load', () => {
     let div = document.getElementById('prompt_here');
     for(let i = 0; i < promptt.length; i++) {
-        let letter = '<span id="targeting' + i + '">' + promptt[i] + '</span>';
+        let letter = '<span style="font-family: Droid Sans Mono; font-weight: 300" class="min-h-[100px] text-[35px] font-bold text-gray-400 rounded-[4px] m-[1px] border-b-2 border-solid border-gray-300" id="targeting' + i + '">' + promptt[i] + '</span>';
         div.innerHTML += letter;  // Append the new span to the div's innerHTML
     }
-    div.setAttribute('class', 'text-5xl font-bold text-gray-400')
+    div.setAttribute('class', '5')
 });
 
-let input = document.getElementById('input_here');
 
 let size = 0;
 let correct = 0;
 let wrong = 0;
 
-input.addEventListener('keydown', (e) => {
+// let input = document.getElementById('input_here');
+window.addEventListener('keydown', (e) => {
     let class_to_target = 'targeting' + size
+    // document.getElementById(class_to_target).style.borderBottomWidth = '5px';
+    // document.getElementById(class_to_target).style.borderBottomColor = 'red';
+
+    // if (size >= 0) {
+    //     // Remove border from the previous character
+    //     let activeClassToTarget = 'targeting' + (size + 1);
+    //     document.getElementById(activeClassToTarget).style.borderBottomWidth = '5px';
+    //     document.getElementById(activeClassToTarget).style.borderBottomColor = 'blue';
+    // }
+
+    // let classToTarget = 'targeting' + size;
+    // let targetElement = document.getElementById(classToTarget);
+
+    // // Apply border to the current character
+    // targetElement.style.borderBottomWidth = '5px';
+    // targetElement.style.borderBottomColor = 'red';
+
     if(e.key === promptt[size]) {
-        console.log('wow')
-        document.getElementById(class_to_target).style.color = 'green';
-        size++;
+        console.log(e.key)
+        document.getElementById(class_to_target).style.color = '#127512';
+        document.getElementById(class_to_target).style.backgroundColor = '#e7fbd3';
+        document.getElementById(class_to_target).style.borderColor = '#e7fbd3';
         correct++;
-    } else {
-        document.getElementById(class_to_target).style.color = 'red';
-        size++;
+    } else if(e.key !== promptt[size]) {
+        document.getElementById(class_to_target).style.color = '#e11d48';
+        document.getElementById(class_to_target).style.backgroundColor = '#ffc0cb';
+        document.getElementById(class_to_target).style.borderColor = '#ffc0cb';
         wrong++;
-    } 
+    }
+    size++;
     if(size == promptt.length) {
         submit_test()
     }
+    
 })
 
 function submit_test() {
-    document.getElementById('results').innerHTML = `<span> Correct: ${correct}, Wrong: ${wrong}`
+    let accuracy = Math.floor((correct/promptt.length)*100)
+    document.getElementById('results').innerHTML = `<span> Accuracy: ${accuracy}%, Correct: ${correct}, Wrong: ${wrong}`
 }
